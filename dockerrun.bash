ddbae9e66a7c2ddf852d25e4b8d25a254b8bfdf3
Zac Flamig <Zac@WeatherWary.com>
2018-07-14 12:18:26 -0500

    blah

--- dockerrun.bash
+++ dockerrun.bash
@@ -2,5 +2,5 @@
 
 cd /var/www/sheepdog
 export PYTHONUNBUFFERED=TRUE
-/usr/local/bin/gunicorn wsgi:app -w 2 -b 127.0.0.1:5000 --log-level=debug --chdir=/var/www/sheepdog --pythonpath "/var/www/sheepdog/,/sheepdog/,/usr/local/lib/python2.7/dist-packages/" --max-requests 5 &
+/usr/local/bin/gunicorn wsgi:app -w 4 -b 127.0.0.1:5000 --log-level=info --chdir=/var/www/sheepdog --pythonpath "/var/www/sheepdog/,/sheepdog/,/usr/local/lib/python2.7/dist-packages/" &
 nginx -g 'daemon off;'
