8dddc7375361b57d083d084c0ec04939159afc60
Zac Flamig <Zac@WeatherWary.com>
2018-07-14 10:26:47 -0500

    blah

--- dockerrun.bash
+++ dockerrun.bash
@@ -2,5 +2,5 @@
 
 cd /var/www/sheepdog
 
-/usr/local/bin/gunicorn wsgi:app -w 4 -b 127.0.0.1:5000 --log-level=debug --chdir=/var/www/sheepdog --pythonpath "-pythonpath = /var/www/sheepdog/,/sheepdog/,/usr/local/lib/python2.7/dist-packages/" &
+/usr/local/bin/gunicorn wsgi:app -w 4 -b 127.0.0.1:5000 --log-level=debug --chdir=/var/www/sheepdog --pythonpath "/var/www/sheepdog/,/sheepdog/,/usr/local/lib/python2.7/dist-packages/" &
 nginx -g 'daemon off;'
